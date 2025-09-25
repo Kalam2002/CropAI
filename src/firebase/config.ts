2c34c4120d7d4ee659ad7c690c4e148a500c1247
Abdul Kalam <abdulkalamjee2021@gmail.com>
2025-09-25 09:31:43 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3808603198-975b2",
+  "appId": "1:932945149879:web:27cd975bc04eca59cd7232",
+  "apiKey": "AIzaSyDMANwXDXjtqiGYvYKKuUfbFALXVqTS3Xc",
+  "authDomain": "studio-3808603198-975b2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "932945149879"
+};
